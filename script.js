b89a662453746656147f627cc1fd270268274802
Donal-Maher <donal.maher.2024@mumail.ie>
2025-02-17 12:37:16 +0000

    working buttons

--- script.js
+++ script.js
@@ -9,3 +9,46 @@
 //     document.getElementById("time").innerHTML = count;
 // }
 // setInterval(countUp, 1000);
+var isStart = false;
+var isPause = false;
+var minutes, seconds;
+minutes = 1;
+seconds = 5;
+function runTimer() {
+    var totalSeconds = minutes * 60 + seconds;
+    if(totalSeconds > 0 && isStart && !isPause) {
+        totalSeconds--;
+    }
+    if(totalSeconds <=60) {
+        document.getElementById("time").style.color = "red";
+        document.getElementById("time").style.backgroundColor = "black";
+        document.getElementById("time").style.borderRadius = "5px";
+    } else {
+        document.getElementById("time").style.color = "black";
+        document.getElementById("time").style.backgroundColor = "white";
+        document.getElementById("time").style.borderRadius = "20px";
+    }
+    minutes = Math.floor(totalSeconds / 60);
+    seconds = totalSeconds % 60;
+    paddedSecs = String(seconds).padStart(2, '0');          //https://codedamn.com/news/javascript/how-to-add-leading-zero-to-a-number-in-javascript
+    paddedMins = String(minutes).padStart(2, '0');
+    document.getElementById("time").innerHTML = paddedMins + " : " + paddedSecs;
+}
+function startButton() {
+    isStart = true;   
+}
+function pauseButton() {
+    if(isPause) {
+        isPause = false;
+        document.getElementById("pauseButton").innerHTML = "Pause";
+    } else {
+        isPause = true;
+        document.getElementById("pauseButton").innerHTML = "Resume";
+    }
+}
+function stopButton() {
+    isStart = false;
+    minutes = 25;
+    seconds = 0;
+}
+setInterval(runTimer, 1000);
